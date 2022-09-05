561a43615e21a482c333e498329c8ada6f7ff594
h-qian1 <2533897869@qq.com>
2022-09-05 16:43:57 +0800

    Changes

---
+++ jdbc02/src/main/java/cn/tedu/Demo03.java
@@ -0,0 +1,17 @@
+package cn.tedu;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Demo03 {
+    public static void main(String[] args) {
+        try (Connection conn = DBUtils.getConn()){
+            Statement s = conn.createStatement();
+            s.executeUpdate("delete from hero where name='猪八戒'");
+            System.out.println("删除完成!");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
