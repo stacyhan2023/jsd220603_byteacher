768252a49644220867e0a02983d75200327189ae
h-qian1 <2533897869@qq.com>
2022-09-06 09:46:05 +0800

    Changes

--- jdbc01/src/main/java/cn/tedu/Demo06.java
+++ jdbc01/src/main/java/cn/tedu/Demo06.java
@@ -1,6 +1,7 @@
 package cn.tedu;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
@@ -25,6 +26,15 @@ create table user(id int primary key auto_increment,username varchar(50),
         try (Connection conn = DBUtils.getConn()){
             //创建执行SQL语句的对象
             Statement s = conn.createStatement();
+            //查询user表中是否已经存在了username
+            ResultSet rs = s.executeQuery(
+                    "select id from user where username='"+username+"'");
+            //判断是否查询到了数据
+            if (rs.next()){
+                System.out.println("用户名已存在");
+                return;
+            }
+
             String sql = "insert into user values(null,'"
                     +username+"','"+password+"','"+nick+"')";
             s.executeUpdate(sql);
