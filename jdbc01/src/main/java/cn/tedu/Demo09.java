88cd535a43470be1959e21e8c34129b0347f5348
h-qian1 <2533897869@qq.com>
2022-09-06 11:44:31 +0800

    Changes

---
+++ jdbc01/src/main/java/cn/tedu/Demo09.java
@@ -0,0 +1,35 @@
+package cn.tedu;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Demo09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入用户名");
+        String username = sc.nextLine();
+        System.out.println("请输入密码");
+        String password = sc.nextLine();
+        System.out.println("请输入昵称");
+        String nick = sc.nextLine();
+        try (Connection conn = DBUtils.getConn()){
+            String sql = "select id from user where username=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1,username);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                System.out.println("用户名已存在!");
+                return;
+            }
+            //往数据库中添加数据
+
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
