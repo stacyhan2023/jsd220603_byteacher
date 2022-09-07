0a27c0b2b3ba1d589d550a8e7fd8e2b614432179
h-qian1 <2533897869@qq.com>
2022-09-07 09:43:50 +0800

    Changes

--- boot02/src/main/java/cn/tedu/boot02/controller/UserController.java
+++ boot02/src/main/java/cn/tedu/boot02/controller/UserController.java
@@ -1,9 +1,15 @@
 package cn.tedu.boot02.controller;
 
 import cn.tedu.boot02.entity.User;
+import cn.tedu.boot02.utils.DBUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Controller
 public class UserController {
@@ -12,7 +18,21 @@ public class UserController {
     @ResponseBody
     public String reg(User user){
         System.out.println("user = " + user);
+        //获取连接
+        try (Connection conn = DBUtils.getConn()){
+            //准备查询数据的SQL
+            String sql = "select id from user where username=?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1,user.getUsername());
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()){
+                return "用户名已存在!";
+            }
 
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
         return "注册成功!";
     }
---
+++ boot02/src/main/java/cn/tedu/boot02/utils/DBUtils.java
@@ -0,0 +1,28 @@
+package cn.tedu.boot02.utils;
+
+import com.alibaba.druid.pool.DruidDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBUtils {
+    private static DruidDataSource dds;
+    static {
+        //创建数据库连接池
+        dds = new DruidDataSource();
+        //设置连接数据库的信息
+        dds.setUrl("jdbc:mysql://localhost:3306/empdb?characterEncoding=utf8&serverTimezone=Asia/Shanghai&useSSL=false");
+        dds.setUsername("root");
+        dds.setPassword("root");
+        //设置初始连接数量
+        dds.setInitialSize(3);
+        //设置最大连接数量
+        dds.setMaxActive(5);
+    }
+    public static Connection getConn() throws SQLException {
+        //获取连接对象  异常抛出
+        Connection conn = dds.getConnection();
+        System.out.println("连接:"+conn);
+        return conn;
+    }
+}
