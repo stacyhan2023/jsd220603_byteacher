69d800391a971a2f74bdaa72d052956f576d6199
h-qian1 <2533897869@qq.com>
2022-09-08 15:26:56 +0800

    Changes

--- boot05/src/main/java/cn/tedu/boot05/controller/AxiosController.java
+++ boot05/src/main/java/cn/tedu/boot05/controller/AxiosController.java
@@ -1,5 +1,7 @@
 package cn.tedu.boot05.controller;
 
+import cn.tedu.boot05.entity.User;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AxiosController {
     @RequestMapping("/helloAxios")
     public String helloAxios(String info){
-
         return "测试成功! info="+info;
+    }
+
+    //如果客户端发出的是异步post请求 并且提交的是自定义JS对象参数
+    // 必须使用@RequestBody注解修饰接收参数的对象
+    @RequestMapping("/postAxios")
+    public String postAxios(@RequestBody User user){
+
+        return "测试成功!"+user.toString();
     }
 
 }
