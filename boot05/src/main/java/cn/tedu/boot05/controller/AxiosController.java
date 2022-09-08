f054a85933ed8d63bb19a4b55c624284f722d11b
h-qian1 <2533897869@qq.com>
2022-09-08 14:49:37 +0800

    Changes

---
+++ boot05/src/main/java/cn/tedu/boot05/controller/AxiosController.java
@@ -0,0 +1,14 @@
+package cn.tedu.boot05.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AxiosController {
+    @RequestMapping("/helloAxios")
+    public String helloAxios(String info){
+
+        return "测试成功! info="+info;
+    }
+
+}
