861f3c3ae84acb2925ea0d65b7a7e18bfbf16e36
h-qian1 <2533897869@qq.com>
2022-09-08 09:32:39 +0800

    Changes

--- boot03/src/main/java/cn/tedu/boot03/controller/ProductController.java
+++ boot03/src/main/java/cn/tedu/boot03/controller/ProductController.java
@@ -26,7 +26,21 @@ public class ProductController {
     @RequestMapping("/select")
     public String select(){
         List<Product> list = mapper.select();
-
-        return list.toString();
+        //把list集合中的数据装进表格标签 响应给客户端
+        String html = "<table border='1'>";
+        html+="<caption>商品列表</caption>";
+        html+="<tr><th>id</th><th>标题</th><th>价格</th><th>库存</th><th>操作</th></tr>";
+        for (Product product:list) {
+            html+="<tr>";
+            html+="<td>"+product.getId()+"</td>";
+            html+="<td>"+product.getTitle()+"</td>";
+            html+="<td>"+product.getPrice()+"</td>";
+            html+="<td>"+product.getNum()+"</td>";
+            // ?是请求地址和请求参数的分隔符
+            html+="<td><a href='/delete?id="+product.getId()+"'>删除</a></td>";
+            html+="</tr>";
+        }
+        html+="</table>";
+        return html;
     }
 }
