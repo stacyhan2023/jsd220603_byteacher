15d4a90076ac4ff7a7a2c7dfdc4555217185a263
h-qian1 <2533897869@qq.com>
2022-09-13 10:51:20 +0800

    Changes

--- boot07/src/main/java/cn/tedu/boot07/controller/UploadController.java
+++ boot07/src/main/java/cn/tedu/boot07/controller/UploadController.java
@@ -4,12 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.UUID;
 
 @RestController
 public class UploadController {
     @RequestMapping("/upload")
-    public void upload(MultipartFile pic){
+    public void upload(MultipartFile pic) throws IOException {
         System.out.println("pic = " + pic);
         //得到文件的原始文件名        a.jpg
         String fileName = pic.getOriginalFilename();
@@ -20,6 +22,21 @@ public class UploadController {
         //得到唯一文件名 UUID.randomUUID() 获取由16进制字符串组成的一个唯一标识符
         fileName = UUID.randomUUID()+suffix;
         System.out.println(fileName);
+        //准备保存图片文件夹的路径
+        String dirPath = "e:/files";
+        File dirFile = new File(dirPath);
+        //判断文件是否存在
+        if (!dirFile.exists()){
+            //创建文件夹
+            dirFile.mkdirs();
+        }
+        //得到保存图片的完整路径 e:/files/xxxx.jpg
+        String filePath = dirPath+"/"+fileName;
+        //把图片文件保存到指定的路径  异常抛出
+        pic.transferTo(new File(filePath));
+
+
+
 
 
     }
