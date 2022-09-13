ca32815ceacd19c34a48844151ee8e43dd8a68b1
h-qian1 <2533897869@qq.com>
2022-09-13 10:38:29 +0800

    Changes

--- boot07/src/main/java/cn/tedu/boot07/controller/UploadController.java
+++ boot07/src/main/java/cn/tedu/boot07/controller/UploadController.java
@@ -4,11 +4,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 @RestController
 public class UploadController {
     @RequestMapping("/upload")
     public void upload(MultipartFile pic){
         System.out.println("pic = " + pic);
+        //得到文件的原始文件名        a.jpg
+        String fileName = pic.getOriginalFilename();
+        System.out.println(fileName);
+        //得到后缀   .jpg
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        System.out.println(suffix);
+        //得到唯一文件名 UUID.randomUUID() 获取由16进制字符串组成的一个唯一标识符
+        fileName = UUID.randomUUID()+suffix;
+        System.out.println(fileName);
+
 
     }
 }
