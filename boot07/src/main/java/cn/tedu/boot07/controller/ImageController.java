1587b9c17ba38018bef7bb7683b3fa9b62a5eb91
h-qian1 <2533897869@qq.com>
2022-09-13 15:55:47 +0800

    Changes

--- boot07/src/main/java/cn/tedu/boot07/controller/ImageController.java
+++ boot07/src/main/java/cn/tedu/boot07/controller/ImageController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,6 +23,8 @@ public class ImageController {
 
     @RequestMapping("/select")
     public List<Image> select(){
+
+
         return mapper.select();
     }
 }
