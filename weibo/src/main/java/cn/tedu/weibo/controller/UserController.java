5a45e570f107aa97e768886915b790a17729e7ef
h-qian1 <2533897869@qq.com>
2022-09-14 09:57:34 +0800

    Changes

--- weibo/src/main/java/cn/tedu/weibo/controller/UserController.java
+++ weibo/src/main/java/cn/tedu/weibo/controller/UserController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class UserController {
     @Autowired
@@ -22,15 +24,24 @@ public class UserController {
          mapper.insert(user);
         return 1;//注册成功!
     }
+
     @RequestMapping("/login")
-    public int login(@RequestBody User user){
+    public int login(@RequestBody User user, HttpSession session){
         User u = mapper.selectByUsername(user.getUsername());
         if (u!=null){
             if (user.getPassword().equals(u.getPassword())){
+                //把当前客户端登录成功的用户对象保存到对应的会话对象中
+                session.setAttribute("user",u);
                 return 1;//登录成功
             }
             return 3;//密码错误
         }
         return 2;//用户名不存在
     }
+    @RequestMapping("/currentUser")
+    public User currentUser(HttpSession session){
+        //从当前客户端对应的会话对象中取出登录成功时保存的用户对象
+        return (User) session.getAttribute("user");
+    }
+
 }
--- weibo/src/main/java/cn/tedu/weibo/mapper/UserMapper.java
+++ weibo/src/main/java/cn/tedu/weibo/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-    @Select("select password from user where username=#{username}")
+    @Select("select id,password,nick from user where username=#{username}")
     User selectByUsername(String username);
 
     @Insert("insert into user values(null,#{username},#{password},#{nick})")
