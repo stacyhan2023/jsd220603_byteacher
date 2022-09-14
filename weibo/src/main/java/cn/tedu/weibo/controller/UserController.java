1882f251141754dbae43fe8aa2503cfadfdb42b9
h-qian1 <2533897869@qq.com>
2022-09-14 09:07:28 +0800

    Changes

--- weibo/src/main/java/cn/tedu/weibo/controller/UserController.java
+++ weibo/src/main/java/cn/tedu/weibo/controller/UserController.java
@@ -22,4 +22,15 @@ public class UserController {
          mapper.insert(user);
         return 1;//注册成功!
     }
+    @RequestMapping("/login")
+    public int login(@RequestBody User user){
+        User u = mapper.selectByUsername(user.getUsername());
+        if (u!=null){
+            if (user.getPassword().equals(u.getPassword())){
+                return 1;//登录成功
+            }
+            return 3;//密码错误
+        }
+        return 2;//用户名不存在
+    }
 }
