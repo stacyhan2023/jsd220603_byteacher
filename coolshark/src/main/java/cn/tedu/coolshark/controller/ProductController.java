a8f8dcb5632098f092c24ef54ae791cf44b23d82
h-qian1 <2533897869@qq.com>
2022-09-16 09:33:27 +0800

    Changes

--- coolshark/src/main/java/cn/tedu/coolshark/controller/ProductController.java
+++ coolshark/src/main/java/cn/tedu/coolshark/controller/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class ProductController {
@@ -19,4 +20,10 @@ public class ProductController {
         System.out.println("product = " + product);
         mapper.insert(product);
     }
+
+    @RequestMapping("/product/select")
+    public List<Product> select(){
+        return mapper.select();
+    }
+
 }
--- coolshark/src/main/java/cn/tedu/coolshark/mapper/ProductMapper.java
+++ coolshark/src/main/java/cn/tedu/coolshark/mapper/ProductMapper.java
@@ -3,6 +3,10 @@ package cn.tedu.coolshark.mapper;
 import cn.tedu.coolshark.entity.Product;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ProductMapper {
@@ -11,4 +15,17 @@ public interface ProductMapper {
             ",#{saleCount},#{num},0,#{created},#{categoryId})")
     void insert(Product product);
 
+    //通过Result注解解决属性名和表字段不一致的情况
+    //    @Select("select id,title,price,old_price,sale_count,url from product")
+//    @Result(column = "old_price",property = "oldPrice")
+//    @Result(column = "sale_count",property = "saleCount")
+//    List<Product> select();
+    //通过别名解决属性名和表字段不一致的情况
+//    @Select("select id,title,price,old_price oldPrice,sale_count saleCount,url from product")
+//    List<Product> select();
+
+    //如果仅仅是因为驼峰命名和_命名规范导致的不一致 可以在application.properties中添加
+    //配置信息解决
+    @Select("select id,title,price,old_price,sale_count,url from product")
+    List<Product> select();
 }
