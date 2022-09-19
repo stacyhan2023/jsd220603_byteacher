7d620d2c7e34797f95d8d2896816b1a1ab77a63f
h-qian1 <2533897869@qq.com>
2022-09-19 11:01:54 +0800

    Changes

--- coolshark/src/main/java/cn/tedu/coolshark/controller/UserController.java
+++ coolshark/src/main/java/cn/tedu/coolshark/controller/UserController.java
@@ -7,19 +7,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 public class UserController {
     @Autowired
     UserMapper mapper;
     @RequestMapping("/login")
-    public int login(@RequestBody User user){
+    public int login(@RequestBody User user, HttpSession session){
         User u = mapper.selectByUsername(user.getUsername());
         if (u!=null){
             if (u.getPassword().equals(user.getPassword())){
+                //把登录成功的用户对象保存到会话里面
+                session.setAttribute("user",u);
                 return 1;
             }
             return 3;
         }
         return 2;
     }
+    @RequestMapping("/currentUser")
+    public User currentUser(HttpSession session){
+        //把登录成功时的用户对象从会话对象中取出响应给客户端
+        return (User) session.getAttribute("user");
+    }
+
 }
