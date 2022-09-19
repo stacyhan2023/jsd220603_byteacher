00c2b39181d384576ad4a7095ab10d810f38c6c0
h-qian1 <2533897869@qq.com>
2022-09-19 14:11:20 +0800

    Changes

--- boot05/src/main/java/cn/tedu/boot05/controller/UserController.java
+++ boot05/src/main/java/cn/tedu/boot05/controller/UserController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.net.HttpCookie;
+
 @RestController
 public class UserController {
     @Autowired
@@ -33,13 +37,23 @@ public class UserController {
         return 1;
     }
     @RequestMapping("/login")
-    public int login(@RequestBody User user){
+    public int login(@RequestBody User user, HttpServletResponse response){
         System.out.println("user = " + user);
-
-
         User u = mapper.selectByUsername(user.getUsername());
         if (u!=null){
             if (user.getPassword().equals(u.getPassword())){
+                //判断是否需要记住用户名和密码
+                if (user.getRem()){
+                    //创建cookie记住用户名
+                    Cookie c1 = new Cookie("username",user.getUsername());
+                    //创建cookie记住密码
+                    Cookie c2 = new Cookie("password",user.getPassword());
+                    //把cookie发送给客户端,交由浏览器保存.
+                    response.addCookie(c1);
+                    response.addCookie(c2);
+                }
+
+
                 return 1;//登录成功
             }
             return 3;//密码错误
