472a998d3d8f7ec2171bac7b1d77dd6e04f4c6d5
h-qian1 <2533897869@qq.com>
2022-09-19 16:31:19 +0800

    Changes

--- coolshark/src/main/java/cn/tedu/coolshark/filter/MyFilter.java
+++ coolshark/src/main/java/cn/tedu/coolshark/filter/MyFilter.java
@@ -1,7 +1,12 @@
 package cn.tedu.coolshark.filter;
 
+import cn.tedu.coolshark.entity.User;
+
 import javax.servlet.*;
 import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(filterName = "MyFilter",urlPatterns = {"/admin.html","/insertProduct.html"})
@@ -15,6 +20,17 @@ public class MyFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         System.out.println("过滤器开始执行");
-        chain.doFilter(request, response);
+        HttpServletRequest rt = (HttpServletRequest) request;
+        HttpServletResponse re = (HttpServletResponse) response;
+        //从请求对象中获取Session
+        HttpSession session = rt.getSession();
+        //获取用户对象
+        User user = (User) session.getAttribute("user");
+        if (user!=null){//登录过
+            //执行此代码才能让客户端请求到资源 不执行代表拦截
+            chain.doFilter(request, response);
+        }else{//未登录
+            re.sendRedirect("/login.html");//重定向到登录页面
+        }
     }
 }
